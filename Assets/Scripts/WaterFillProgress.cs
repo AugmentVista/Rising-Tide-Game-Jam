f62b35bc22a56d6786110f8a574935e140679aeb
AugmentVista <AuAgments@gmail.com>
2025-05-03 03:40:40 -0300

    Simple Wave fill script added
    The wave image increases fill amount relative to score. It advances to wave 2, wave 3 and so on when the next threshold is reached.

--- Assets/Scripts/WaterFillProgress.cs
+++ Assets/Scripts/WaterFillProgress.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class WaterFillProgress : MonoBehaviour
 {
     public Image waterImage;
 
     public ScoreDisplay scoreDisplay;
+
+    public TextMeshProUGUI waveCount;
+
+    private int waveCountInt = 0;
+
+    private float threshold = 10000f;
 
 
     void Start()
@@ -16,6 +23,13 @@ public class WaterFillProgress : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        waterImage.fillAmount = scoreDisplay.publicScore / 10000f;
+        waterImage.fillAmount = scoreDisplay.publicScore / threshold;
+
+        if (waterImage.fillAmount == 1.0f)
+        {
+            waveCountInt++;
+            waveCount.text = "Wave " + waveCountInt.ToString();
+            threshold *= 3;
+        }
     }
 }
