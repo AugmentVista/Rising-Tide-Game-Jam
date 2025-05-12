1ab28c742e230329bd0e23073ca38288d036db56
Silver Burnie <AuAgments@gmail.com>
2025-05-12 01:52:14 -0300

    Wave blend implemented

--- Assets/Scripts/Boat Scripts/WaterFillProgress.cs
+++ Assets/Scripts/Boat Scripts/WaterFillProgress.cs
@@ -9,39 +9,75 @@ public class WaterFillProgress : MonoBehaviour
     public Slider boatSlider;
     public ScoreDisplay scoreDisplay;
     public TextMeshProUGUI waveCount;
+    public Animator boatAnimator;
 
     private int waveCountInt = 0;
-    private float threshold = 100f;
+    private float threshold = 5f;
+    private int clickCount = 0;
+
+    private int delay = 5000;
+    private int timer = 0;
+    private float addBlend = 0.0f;
+
+    public int weight = 1;
+
     private List<string> shipNames = new List<string>();
 
     void Start()
     {
         waterImage.fillAmount = 0f;
         boatSlider.value = 0f;
-        shipNames.Add("Sloop");
-        shipNames.Add("Schooner");
-        shipNames.Add("Brigantine");
-        shipNames.Add("Galleon");
-        shipNames.Add("Man-o’-war");
+        shipNames.Add("The Classic");
+        shipNames.Add("Sad cat");
+        shipNames.Add("Lawnchair");
+        shipNames.Add("The half-boat");
+        shipNames.Add("Pet Rock");
         shipNames.Add("Super Sloop");
         shipNames.Add("Super Schooner");
         shipNames.Add("Super Brigantine");
         shipNames.Add("Super Galleon");
         shipNames.Add("Super Man-o’-war");
+        scoreDisplay.shipName = shipNames[waveCountInt].ToString();
     }
+
+    /// <summary>
+    /// Only clicking on boat should call this
+    /// </summary>
+    /// <param name="amount"></param>
+    public void FillWater(int amount)
+    {
+        clickCount += amount;
+        waterImage.fillAmount = clickCount / threshold;
+    }
+
+    public void DrainWater(int amount)
+    {
+        clickCount -= amount;
+        waterImage.fillAmount = clickCount / threshold;
+    }
+
 
     void Update()
     {
-        waterImage.fillAmount = scoreDisplay.publicScore / threshold;
-        boatSlider.value = waterImage.fillAmount;
+        timer++;
+        if (timer > delay)
+        {
+            DrainWater(weight);
+            timer = 0;
+        }
 
-        scoreDisplay.shipName = shipNames[waveCountInt].ToString();
+        boatSlider.value = waterImage.fillAmount;
 
         if (waterImage.fillAmount >= 1.0f)
         {
             waveCountInt++;
             waveCount.text = "Wave " + waveCountInt.ToString();
             threshold *= 3;
+            waterImage.fillAmount = 0.0f;
+            clickCount = 0;
+            addBlend += 0.25f;
+            boatAnimator.SetFloat("Blend", addBlend);
+            scoreDisplay.shipName = shipNames[waveCountInt].ToString();
         }
     }
 }
