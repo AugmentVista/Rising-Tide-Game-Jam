4a5d4af5735a6247e886fca8dcc9f14c498a3a76
Silver Burnie <AuAgments@gmail.com>
2025-05-16 20:25:45 -0300

    credits button fixed

--- Assets/Scripts/Jasons Button Scripts/CreditsButtons.cs
+++ Assets/Scripts/Jasons Button Scripts/CreditsButtons.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class CreditsButtons : MonoBehaviour
 {
-
-
     public void PixaBayCardoard()
     {
         Application.OpenURL("https://pixabay.com/photos/parcel-post-deliver-moving-4967721/");
@@ -14,5 +12,10 @@ public class CreditsButtons : MonoBehaviour
     public void PixaBayBackgroundMusic()
     {
         Application.OpenURL("https://pixabay.com/music/video-games-game-music-teste-204327/");
+    }
+
+    public void PixabayBoatButtonSound()
+    {
+        Application.OpenURL("https://pixabay.com/sound-effects/water-drip-45622/");
     }
 }
