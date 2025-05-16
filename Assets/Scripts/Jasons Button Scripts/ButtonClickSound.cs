a865cd263349f63003ebbcfc04a015b2439115b4
Silver Burnie <AuAgments@gmail.com>
2025-05-16 18:18:24 -0300

    Audio issues resolved, sync test for sarah

--- Assets/Scripts/Jasons Button Scripts/ButtonClickSound.cs
+++ Assets/Scripts/Jasons Button Scripts/ButtonClickSound.cs
@@ -6,13 +6,34 @@ public class ButtonClickSound : MonoBehaviour
 {
     [Header("Click Sounds & Source")]
     public List<AudioClip> audioClips;
+    public AudioClip SaraSound;
+    public List<AudioClip> Boatclick;
     public AudioSource GameplaySounds;
 
     public void BOnClick()
     {
         int rnd = Random.Range(0, audioClips.Count);
-        GameplaySounds.Stop();
-        GameplaySounds.clip = audioClips[rnd];
+        int SaraRND = Random.Range(0, 101);
+
+        if (SaraRND == 7)
+        {
+            GameplaySounds.Stop();
+            GameplaySounds.clip = SaraSound;
+            GameplaySounds.Play();
+        }
+
+        else
+        {
+            GameplaySounds.Stop();
+            GameplaySounds.clip = audioClips[rnd];
+            GameplaySounds.Play();
+        }
+    }
+
+    public void BBoatButton()
+    {
+        int rnd = Random.Range(0, Boatclick.Count);
+        GameplaySounds.clip = Boatclick[rnd];
         GameplaySounds.Play();
     }
 }
